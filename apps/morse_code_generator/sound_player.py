7f9d8052d5df36ac15a8c65b8f79546a3259a33e
dmitrydell <igetsemails@gmail.com>
2024-09-02 23:19:23 -0500

    update sound_player.py in morse_code_generator to only use pydub

--- apps/morse_code_generator/sound_player.py
+++ apps/morse_code_generator/sound_player.py
@@ -1,16 +1,17 @@
-import pygame
-import os
 from pydub import AudioSegment
+import os
+
 
 class SoundPlayer():
 
     def __init__(self):
-        pygame.mixer.init()  # Initialize the mixer once
-        # Load the .wav file
+        # Load the .wav files
         file_path_dot_wav = os.path.join('apps', 'morse_code_generator', 'static', 'dot.wav')
         file_path_dash_wav = os.path.join('apps', 'morse_code_generator', 'static', 'dash.wav')
         file_path_silence_wav = os.path.join('apps', 'morse_code_generator', 'static', 'silence.wav')
         self.file_path_output_wav = os.path.join('apps', 'morse_code_generator', 'generated_sound')
+
+        # Load sounds using Pydub
         self.sound_dot = AudioSegment.from_wav(file_path_dot_wav)
         self.sound_dash = AudioSegment.from_wav(file_path_dash_wav)
         self.sound_silence = AudioSegment.from_wav(file_path_silence_wav)
@@ -19,14 +20,14 @@ class SoundPlayer():
         combined_sound = AudioSegment.empty()
 
         for char in morse_code:
-
             if char == '.':
                 combined_sound += self.sound_dot + self.sound_silence
-
             elif char == '-':
                 combined_sound += self.sound_dash + self.sound_silence
-
             else:
                 combined_sound += self.sound_silence * 3
 
-        combined_sound.export(f'{self.file_path_output_wav}/{output_file}', format="wav")  # Export the combined sound to MP3
+        # Ensure output directory exists
+        os.makedirs(self.file_path_output_wav, exist_ok=True)
+
+        combined_sound.export(f'{self.file_path_output_wav}/{output_file}', format="wav")
